8ac7c1995c2ad8f7fc46efad885630c6a5d0802c
m-amalgama <m.zamoshkin.dev@gmail.com>
2026-07-03 03:30:22 +0200

    add 3 buttons and handler for the first button (нопка 1)

--- bot_learn.py
+++ bot_learn.py
@@ -1,15 +1,28 @@
 import asyncio
+from aiogram import F
 from aiogram import Bot, Dispatcher, Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from config import settings
 
 router = Router()
+kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Кнопка 1"), KeyboardButton(text="Кнопка 2")],
+        [KeyboardButton(text="Кнопка 3")]
+    ],
+    resize_keyboard=True
+)
+
+@router.message(F.text == "Кнопка 1")
+async def button_1_handler(msg_button_1:Message):
+    await msg_button_1.answer("первая кнопка")
+    
 
 @router.message(CommandStart())
-async def start_handler(message: Message):
-    await message.answer("hello")
+async def start_handler(msg_start: Message):
+    await msg_start.answer("hello", reply_markup=kb)
 
 async def main():
     bot = Bot(token=settings.token)
