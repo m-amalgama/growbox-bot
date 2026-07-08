96d6767ccc0d6cb4c6b32ef6de62a22a9cb2600b
m-amalgama <m.zamoshkin.dev@gmail.com>
2026-07-08 03:53:04 +0200

    add proportional fan control on mock + temp/speed plot

---
+++ Logik.py
@@ -0,0 +1,51 @@
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def fun(temp):
+    base = 45
+    K = 10
+    error = temp - 27
+    speed = min((base + error * K), 80)
+    return speed
+
+def mock_1():
+    temp = round(random.uniform(25, 34), 2)
+    humi = round(random.uniform(40, 55), 2)
+    svp = 0.61078 * math.exp((17.27 * temp) / (temp + 237.3))
+    avp = svp * (humi / 100.0)
+    vpd = svp - avp
+    return {"temp": temp, "humi": humi, "vpd": vpd}
+
+def mock_n():
+    v_list = []
+    count = 0
+    base_temp = 25.0
+    base_humi = 40.2
+    while count < 100:
+        dict_t_h_v = {}
+        temp = base_temp + (count * 0.1)
+        humi = min(base_humi + (count * 0.1), 100)
+        svp = 0.61078 * math.exp((17.27 * temp) / (temp + 237.3))
+        avp = svp * (humi / 100.0)
+        vpd = svp - avp
+        dict_t_h_v["temp"] = round(temp, 2)
+        dict_t_h_v["humi"] = round(humi, 2)
+        dict_t_h_v["vpd"] = round(vpd, 3)
+        v_list.append(dict_t_h_v)
+        count += 1
+    return v_list
+
+mock = mock_n()
+temps = [t["temp"] for t in mock]
+speeds = [fun(t) for t in temps]
+plt.plot(temps, speeds)
+plt.xlabel("temp")
+plt.ylabel("speed %")
+plt.show()
+print(temps)
+print(speeds)
+
+
