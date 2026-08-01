386e008a971b116d79002ddf1a789a48d30bad56
m-amalgama <m.zamoshkin.dev@gmail.com>
2026-08-01 02:30:56 +0200

    fix: restore error sign

--- Ven_fun.py
+++ Ven_fun.py
@@ -1,6 +1,6 @@
 def ven(box_temp,target,max_speed):
     base = 45
     K = 10
-    error = target - box_temp
+    error = box_temp - target
     speed = max(min((base + error * K), max_speed), 0)
     return int(speed)
