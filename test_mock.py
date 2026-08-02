0aa76a6845eaa2ea4a832ca98de90b11730635d2
m-amalgama <m.zamoshkin.dev@gmail.com>
2026-08-02 20:22:22 +0200

    fixture dfor test_mock

--- test_mock.py
+++ test_mock.py
@@ -1,14 +1,15 @@
+import pytest
 from Mock import mock_1
 
-def test_mock_key_in_dict():
-    d = mock_1()
+@pytest.fixture
+def d():
+    return mock_1()
+
+def test_mock_key_in_dict(d):
     assert "temp" in d            
-def test_len_dict():
-    d = mock_1()
+def test_len_dict(d):
     assert len(d) == 3           
-def test_type_temp():
-    d = mock_1()
+def test_type_temp(d):
     assert isinstance(d["temp"], float)  
-def test_temp_in_sensor_range():
-    d = mock_1()
+def test_temp_in_sensor_range(d):
     assert 25 <= d["temp"] <= 34 
